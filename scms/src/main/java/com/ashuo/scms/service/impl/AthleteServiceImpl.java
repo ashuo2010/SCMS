4be3adf642c4f9061ccb70f47722594f8be25599
AShuo <1253788988@qq.com>
2021-09-08 10:55:32 +0800

    运动员报名时增加本届报名人数

--- scms/src/main/java/com/ashuo/scms/service/impl/AthleteServiceImpl.java
+++ scms/src/main/java/com/ashuo/scms/service/impl/AthleteServiceImpl.java
@@ -3,9 +3,11 @@ package com.ashuo.scms.service.impl;
 import com.ashuo.scms.dto.AthleteScoreDto;
 import com.ashuo.scms.entity.Athlete;
 import com.ashuo.scms.entity.Item;
+import com.ashuo.scms.entity.Season;
 import com.ashuo.scms.mapper.AthleteMapper;
 import com.ashuo.scms.mapper.ItemMapper;
 import com.ashuo.scms.service.AthleteService;
+import com.ashuo.scms.service.SeasonService;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +32,9 @@ public class AthleteServiceImpl implements AthleteService {
     @Autowired
     ItemMapper itemMapper;
 
+    @Autowired
+    SeasonService seasonService;
+
     @Override
     public IPage<Athlete> getAthleteByCondition(Page<Athlete> page, Athlete athleteCondition) {
         if (athleteCondition == null) {
@@ -50,7 +55,11 @@ public class AthleteServiceImpl implements AthleteService {
             Item item = itemMapper.queryItemByItemCondition(athlete.getItem());
             item.setAthleteAmount(item.getAthleteAmount() + 1);
             int effNum2 = itemMapper.updateItem(item);
-            if (effNum != 1 || effNum2 != 1) {
+            //将本届运动会的报名人数增加一
+            Season season = seasonService.getSeasonById(item.getSeason());
+            season.setSeasonAthleteAmount(season.getSeasonAthleteAmount());
+            int effNum3 = seasonService.modifySeason(season);
+            if (effNum != 1 || effNum2 != 1 || effNum3 != 1) {
                 return 0;
             } else {
                 return effNum;
@@ -83,11 +92,15 @@ public class AthleteServiceImpl implements AthleteService {
             IPage<Athlete> athleteList = athleteMapper.queryAthleteByAthleteCondition(page, athlete);
             athlete = athleteList.getRecords().get(0);
             Item item = itemMapper.queryItemByItemCondition(athlete.getItem());
-            //将对应项目报名数量增减一
+            //将对应项目报名数量减一
             item.setAthleteAmount(item.getAthleteAmount() - 1);
             int effNum = itemMapper.updateItem(item);
             int effNum2 = athleteMapper.deleteAthlete(athleteId);
-            if (effNum <= 0 || effNum2 <= 0) {
+            //将本届运动会的报名人数减一
+            Season season = seasonService.getSeasonById(item.getSeason());
+            season.setSeasonAthleteAmount(season.getSeasonAthleteAmount());
+            int effNum3 = seasonService.modifySeason(season);
+            if (effNum <= 0 || effNum2 <= 0 || effNum3 <= 0) {
                 return 0;
             } else {
                 return effNum;
